196ef46314d7ca3c87d30e4498f79b7a07a77cb1
RickyY1689 <richard.jr.yang@gmail.com>
2021-05-01 23:10:17 -0400

    Fixed redundant checkpoint restoration

--- main.py
+++ main.py
@@ -2,23 +2,32 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 from flask import Flask, abort, jsonify, request
-from flask_cors import CORS, cross_origin
-from tensorflow import keras
+#from flask_cors import CORS, cross_origin
+import tensorflow as tf
 import gpt_2_simple as gpt2
-from .run_generation import generate_text
+
+if not os.path.exists('models'):
+    gpt2.download_gpt2(model_name="355M")
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-gpt2.download_gpt2(model_name="355M")
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
+#keras.backend.clear_session()d
+print("GPT downloaded")
+sess = gpt2.start_tf_sess()
+print("session started")
+gpt2.load_gpt2(sess, run_name='run1')
+print("checkpoint loaded")
+
+graph = tf.get_default_graph()
 
 def getResponse(msg, prefixHistory):
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name='run1')
+    global graph
     prefixHistory += "Person: " + msg + "\n" + "Aryeh Bookbinder:"
     print(prefixHistory)
-    text = gpt2.generate(sess, length=200,temperature=0.6,prefix=prefixHistory, return_as_list=True)[0]
+    with graph.as_default():
+        text = gpt2.generate(sess, length=25,temperature=0.7,prefix=prefixHistory, return_as_list=True)[0]
     print(prefixHistory,  text)
     text = text[len(prefixHistory):]
     print(prefixHistory,  text)
@@ -28,7 +37,7 @@ def getResponse(msg, prefixHistory):
     return (text[:end-1]), prefixHistory
 
 @app.route("/generate", methods=['POST'])
-@cross_origin()
+#@cross_origin()
 def get_gen():
     data = request.get_json()
 
@@ -45,7 +54,6 @@ def get_gen():
         #     model_name_or_path=model
         # )
         response, _ = getResponse(data['text'], data['prefixHistory'])
-        print(response)
         #result = response.replace("Aryeh Bookbinder", "Aryeh Bot")
         return jsonify({'result': "Aryeh Bookbinder:" + str(response)})
 
